e2f8ecfbbf0b0d3128063d935181ebc8d080bcaa
Demo User <demouser@dcloud.cisco.com>
2020-01-30 05:16:50 +0000

    commit

--- python_hands_on.py
+++ python_hands_on.py
@@ -8,26 +8,37 @@ import random
 # TODO 1
 # Write a print statement that displays both the type and value of `pi`
 pi = math.pi
+print(type(pi), pi)
 
 
 # TODO 2
 # Write a conditional to print out if `i` is less than or greater than or equal to 50
 i = random.randint(0, 100)
-
-
+if i < 50:
+    print ('i is less than 50')
+elif i > 50:
+    print ('i is greater than 50')
+else:
+    print ('i is equal to 50')
 # TODO 3
 # Write a conditional that prints the color of the picked fruit
 picked_fruit = random.choice(['orange', 'strawberry', 'banana'])
-
+if picked_fruit == 'orange':
+    print ('orange')
+elif picked_fruit == 'strawberry':
+    print ('red')
+else :
+    print ('yellow')
 
 # TODO 4
 # Write a function that multiplies two numbers and returns the result
 # Define the function here.
-
+def multiply(num1, num2):
+    return num1 * num2
 
 # TODO 5
 # use `input` function to get 2 number and print the multiply of numbers.
-num1 = 
-num2 = 
+num1 = int(input('enter number1:'))
+num2 = int(input('enter number2:'))
 
-print("multiply answer =", )
+print("multiply answer =", multiply(num1, num2))
